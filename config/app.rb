9aaeb84b0cdbb6648522bde867ee28e77d31de67
Brian Foshee <brianfoshee@gmail.com>
2013-10-31 00:21:40 -0400

    Modified model for hstore. Data Parsing properly
    Model contains all hstore attributes that should be
    set. Sinatra endpoint properly iterates all data
    and stores it.

--- config/app.rb
+++ config/app.rb
@@ -4,6 +4,11 @@ require 'sinatra/activerecord'
 set :database, "postgres://localhost:5432/solar-collector-dev"
 
 class Reading < ActiveRecord::Base
+  store_accessor :data, :Sweep_Pmax, :Ahc_r, :Vb_ref, :Ahc_t, :Power_out,
+  :adc_va_f, :T_amb, :T_hs, :Ahl_r, :adc_vb_f, :Sweep_Vmp, :Vb_min_daily,
+  :Vb_max_daily, :Ahl_t, :kWhc, :hourmeter, :adc_ic_f, :adc_vl_f, :Sweep_Voc,
+  :adc_il_f, :charge_state, :Ahl_daily, :Ahc_daily, :Vb_f, :V_lvd
+
   validates :read_time, presence: :true
   validates :data, presence: :true
 end
@@ -12,23 +17,21 @@ post '/readings' do
   content_type :json
   reading = request.body.read
   data = JSON.parse(reading)
-  # TODO: refactor this for proper input format
-  time = data.delete('read_time')
-  time = Time.at(time) if !time.nil?
-  r = Reading.new(data: data, read_time: time)
-  if r.save
-    r.data.to_s
-  else
-    "Could not save #{r.errors.messages}"
+  old, knew = 0, 0
+  data.each do |k,v|
+    label = k
+    v.each do |k,v|
+      t = Time.at(k.to_i).to_s
+      tstamp = DateTime.parse(t)
+      r = Reading.find_or_create_by(read_time: tstamp)
+      r.send("#{label.to_sym}=", v)
+      if r.new_record?
+        knew += 1
+      else
+        old +=1
+      end
+      r.save!
+    end
   end
+  "{ data: { old: #{old}}, new: #{knew} }"
 end
-
-# Format is:
-# {
-#   "Ahc_daily": {
-#       "1380908350": 9.8,
-#       "1380908355": 9.8,
-#       "1380908399": 9.8,
-#       "1380908404": 9.8
-#     }
-# }
